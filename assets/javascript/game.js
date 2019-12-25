592f7f5ea650a4a7791ee69475e5c6421fd91161
Sean Jacobson <sean.jacobson86@gmail.com>
2019-12-24 18:00:51 -0700

    I added some code that will show when the user selects certain characters. I got it to work after a little while.

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+//I'm going to need to be able to click on certain characters
+//I'm just going to test it out for now
+$("#lukeClick").click(function() {
+    console.log("You've selected Luke.");
+})
+
+$("#leiaClick").click(function() {
+    console.log("You've selected Leia.");
+})
+
+$("#darth-vaderClick").click(function() {
+    console.log("You've selected Darth Vader.");
+})
+
+$("#emperor-palpatineClick").click(function() {
+    console.log("You've selected Emperor Palpatine.");
+})
