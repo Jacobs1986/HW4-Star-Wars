e7fc27c823d01d8f7ca38227b5f6825d31bf0776
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-02 16:54:22 -0700

    I added a conditional statement so that when the player chooses a character the first time around the other characters will be moved to enemies area.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,54 @@
 //Universal variables
 let hero;
-let denfender;
+let defender;
+console.log(hero)
 
 //First I need a code that will display the character that is selected
 
 $("#luke-click").click(function () {
-    console.log("You have selected Luke.")
-    clickMove("leia");
-    clickMove("vader");
-    clickMove("palpatine");
+    if (hero == undefined) {
+        hero = "luke";
+        console.log("You have selected Luke.")
+        clickMove("leia");
+        clickMove("vader");
+        clickMove("palpatine");
+    }
 })
 
 $("#leia-click").click(function () {
-    console.log("You have selected Leia.")
-    clickMove("luke");
-    clickMove("vader");
-    clickMove("palpatine");
+    if (hero == undefined) {
+        hero = "leia";
+        console.log("You have selected Leia.")
+        clickMove("luke");
+        clickMove("vader");
+        clickMove("palpatine");
+    }
 })
 
 $("#vader-click").click(function () {
-    console.log("You have selected Darth Vader.")
-    clickMove("luke");
-    clickMove("leia");
-    clickMove("palpatine");
+    if (hero == undefined) {
+        hero = "vader";
+        console.log("You have selected Darth Vader.")
+        clickMove("luke");
+        clickMove("leia");
+        clickMove("palpatine");
+    }
 })
 
 $("#palpatine-click").click(function () {
-    console.log("You have selected Paplaptine.")
-    clickMove("luke");
-    clickMove("leia");
-    clickMove("vader");
+    if (hero == undefined) {
+        hero = "palpatine";
+        console.log("You have selected Paplaptine.")
+        clickMove("luke");
+        clickMove("leia");
+        clickMove("vader");
+    }
 })
-
 
 //function to move the character clicks, their names and health.
 function clickMove(character) {
-   $(`#${character}-click`).appendTo(".enemies");
-   $(`#${character}-name`).appendTo(".enemy-names");
-   $(`#${character}-health`).appendTo(".enemy-health");
-   $(`#${character}-pic`).css("border-color", "red");
+    $(`#${character}-click`).appendTo(".enemies");
+    $(`#${character}-name`).appendTo(".enemy-names");
+    $(`#${character}-health`).appendTo(".enemy-health");
+    $(`#${character}-pic`).css("border-color", "red");
 }
